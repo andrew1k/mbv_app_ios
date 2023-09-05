3cfb81e6ac514aa92db6c3585e4794a4233be4ab
Andrew McKo <andrewkoshelev@hotmail.com>
2023-09-05 13:34:09 +0300

    Added all

---
+++ App/App/public/assets/PrayerPage-ded81f61.js
@@ -0,0 +1,10 @@
+import{P as s}from"./PurposesCards-a691f298.js";import{r as o,o as c,c as b,w as r,V as a,a as e,u as n}from"./index-a1372ae8.js";const u="/assets/PrayerWorship-d7a6529a.jpg",i={__name:"PrayerPage",setup(m){const t=o(`<h2>Вечера молитвы и поклонения</h2><br/>
+- По средам в 19:00 в Малом Зале Церкви <br><br>
+<h2>Молодежная молитва</h2><br>
+- Последняя среда месяца в 19:00 в Малом Зале Церкви<br><br>
+<h2>День поста</h2><br>
+Первая среда месяца <br>
+- Утренняя молитва в 10:00 в Малом Зале Церкви <br>
+- Вечерняя межцерковная молитва в 19:00 в Большом Зале Церкви <br>
+- Суточный пост со вторника 21:00 до среды 21:00 <br><br>
+`);return(p,_)=>(c(),b(a,{rounded:"0",variant:"text"},{default:r(()=>[e(a,{class:"ma-2"},{default:r(()=>[e(s,{img:n(u),text:t.value},null,8,["img","text"])]),_:1})]),_:1}))}};export{i as default};
